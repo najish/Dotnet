63253d7cca1a3b0d046baf108c1f7cee12cd9ad4
zafer eqbal <najish.eqbal@gmail.com>
2022-07-22 04:40:30 +0530

    Prev and Next Page Functionlity

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -10,13 +10,17 @@ namespace Dotnet.Controllers;
 public class StudentController : Controller
 {
     private readonly IStudentRepository studentRepo;
+    private static int currentPage = 1,LastPage;
+    private const int dataPerPage = 5, FirstPage = 1;
+    private static int totalPage;
     public StudentController(IStudentRepository studentRepo)
     {
         this.studentRepo = studentRepo;
     }
 
+    [HttpGet]
     [AllowAnonymous]
-    public async Task<IActionResult> GetStudents(string sortOrder,string searchString)
+    public async Task<IActionResult> GetStudents(string sortOrder,string searchString,int page = 0)
     {
         ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
         ViewData["AddressSortParm"] = String.IsNullOrEmpty(sortOrder) ? "address_desc" : "";
@@ -24,9 +28,12 @@ public class StudentController : Controller
         ViewData["CurrentFilter"] = searchString;
         var list = await studentRepo.GetStudentsAsync();
         var students = from student in list
-                       where student.Name.Contains(searchString)
                        select student;
 
+        if(String.IsNullOrEmpty(searchString) == false)
+        {
+            students = students.Where(std => std.Name.Contains(searchString));
+        }
         switch(sortOrder)
         {
             case "id_desc":
@@ -42,6 +49,32 @@ public class StudentController : Controller
                 students = students.OrderBy(x => x.Id);
                 break;
         }
+        int total = students.Count();
+        LastPage = (int)Math.Floor((total * 1.0 ) / dataPerPage);
+        if(page > currentPage && page <= LastPage)
+        {
+            currentPage = page;
+            students = students.Skip(dataPerPage * page);
+            if(students.Count() >= dataPerPage)
+                students = students.Take(dataPerPage);
+        }
+        else if(page < currentPage && page >= FirstPage)
+        {
+            currentPage = page;
+            students = students.Skip((page-1) * dataPerPage);
+            if(students.Count() >= dataPerPage)
+                students = students.Take(dataPerPage);
+        }
+        else if(page > LastPage)
+        {
+            page = 0;
+        }
+        if(page == 0) page = currentPage;
+        ViewData["page"] = page;
+        currentPage = page;
+
+        if(students.Count() > dataPerPage)
+            students = students.Take(dataPerPage);
         return View(students);
     }
 
